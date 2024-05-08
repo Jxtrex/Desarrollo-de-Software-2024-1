79af756b2662ef2c4d1637c19e1c80767978debc
Lerzundi <juan.lerzundi.r@uni.pe>
2024-05-08 08:59:06 -0500

    Update Ejercicio 2

--- ExamenParcial-CC3S2/Ejercicio 2/Sprint1/src/test/java/WordTest.java
+++ ExamenParcial-CC3S2/Ejercicio 2/Sprint1/src/test/java/WordTest.java
@@ -1,34 +1,56 @@
+import com.wordz.domain.Letter;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static com.wordz.domain.Letter.*;
-
 public class WordTest {
+
     @Test
     public void oneIncorrectLetter() {
+        // Arrange
         var word = new Word("A");
+
+        // Act
         var score = word.guess("Z");
+
+        // Assert
         assertScoreForGuess(score, INCORRECT);
     }
 
     @Test
     public void oneCorrectLetter() {
+        // Arrange
         var word = new Word("A");
+
+        // Act
         var score = word.guess("A");
+
+        // Assert
         assertScoreForGuess(score, CORRECT);
     }
 
     @Test
     public void secondLetterWrongPosition() {
+        // Arrange
         var word = new Word("AR");
+
+        // Act
         var score = word.guess("ZA");
+
+        // Assert
         assertScoreForGuess(score, INCORRECT, PART_CORRECT);
     }
 
     @Test
     public void allScoreCombinations() {
+
+        // Arrange
         var word = new Word("ARI");
+
+        // Act
         var score = word.guess("ZAI");
+
+        // Assert
         assertScoreForGuess(score, INCORRECT, PART_CORRECT, CORRECT);
     }
 
